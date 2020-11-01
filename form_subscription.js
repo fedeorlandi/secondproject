52ee3d6cb68f4276191de9a046221c28c1a71be6
fedeorlandi4@gmail.com <fedeorlandi4@gmail.com>
2020-11-01 16:07:03 -0300

    message of error modified

--- form_subscription.js
+++ form_subscription.js
@@ -1,4 +1,4 @@
-var formulario =document.getElementById('form');
+var formulario = document.getElementById('form');
 var inputs = document.querySelectorAll('#form input')
 var expresions = {
     completename: /^[a-zA-ZÀ-ÿ\s]{6,40}$/,
@@ -12,6 +12,18 @@ var expresions = {
     identidy: /^\d{7,8}$/
 }
 
+var fields = {
+    completename: false,
+    correo: false,
+    password: false,
+    ages: false,
+    phone: false,
+    nameadress: false,
+    capitacity: false,
+    codepostal: false,
+    identidy: false
+}
+
 
 
 
@@ -21,10 +33,54 @@ var validateform2 = function validatecampo2(e){
             document.getElementById('group_namecomplete').classList.remove('form_group-wrong');
             document.getElementById('group_namecomplete').classList.remove('form_group-correct'); 
             document.querySelector('#group_namecomplete .form_input-error').classList.remove('form_input-error-active');
-            
+        break;
+        case "email":
+            document.getElementById('group_email').classList.remove('form_group-wrong');
+            document.getElementById('group_email').classList.remove('form_group-correct'); 
+            document.querySelector('#group_email .form_input-error').classList.remove('form_input-error-active');
+        break;
+        case "password":
+            document.getElementById('group_password').classList.remove('form_group-wrong');
+            document.getElementById('group_password').classList.remove('form_group-correct'); 
+            document.querySelector('#group_password .form_input-error').classList.remove('form_input-error-active');
+        break;
+        case "password2":
+            document.getElementById('group_password2').classList.remove('form_group-wrong');
+            document.getElementById('group_password2').classList.add('form_group-correct');
+            document.querySelector('#group_password2 .form_input-error').classList.remove('form_input-error-active');
+        break;
+        case "age":
+            document.getElementById('group_age').classList.remove('form_group-wrong');
+            document.getElementById('group_age').classList.remove('form_group-correct');
+            document.querySelector('#group_age .form_input-error').classList.remove('form_input-error-active');
+        break;
+        case "TelephoneNumber":
+            document.getElementById('group_TelephoneNumber').classList.remove('form_group-wrong');
+            document.getElementById('group_TelephoneNumber').classList.remove('form_group-correct'); 
+            document.querySelector('#group_TelephoneNumber .form_input-error').classList.remove('form_input-error-active');
+        break;
+        case "Adress":
+            document.getElementById('group_Adress').classList.remove('form_group-wrong');
+            document.getElementById('group_Adress').classList.remove('form_group-correct'); 
+            document.querySelector('#group_Adress .form_input-error').classList.remove('form_input-error-active');
+        break;
+        case "city":
+            document.getElementById('group_city').classList.remove('form_group-wrong');
+            document.getElementById('group_city').classList.remove('form_group-correct'); 
+            document.querySelector('#group_city .form_input-error').classList.remove('form_input-error-active');
+        break;
+        case "postalcode":
+            document.getElementById('group_postalcode').classList.remove('form_group-wrong');
+            document.getElementById('group_postalcode').classList.remove('form_group-correct'); 
+            document.querySelector('#group_postalcode .form_input-error').classList.remove('form_input-error-active');
+        break;
+        case "dni":
+            document.getElementById('group_dni').classList.remove('form_group-wrong');
+            document.getElementById('group_dni').classList.remove('form_group-correct'); 
+            document.querySelector('#group_dni .form_input-error').classList.remove('form_input-error-active');
+        break;            
     }
 }
-
 
 
 
@@ -35,10 +91,13 @@ var validateform = function validatecampo(e){
                 document.getElementById('group_namecomplete').classList.remove('form_group-wrong');
                 document.getElementById('group_namecomplete').classList.add('form_group-correct'); 
                 document.querySelector('#group_namecomplete .form_input-error').classList.remove('form_input-error-active');
+                fields.completename = true;
+                //fullname = document.getElementById('namecomplete').value;
             }else {
                 document.getElementById('group_namecomplete').classList.add('form_group-wrong');
                 document.getElementById('group_namecomplete').classList.remove('form_group-correct');
                 document.querySelector('#group_namecomplete .form_input-error').classList.add('form_input-error-active');
+                fields.completename = false;
             }     
         break;
         case "email":
@@ -46,10 +105,12 @@ var validateform = function validatecampo(e){
                 document.getElementById('group_email').classList.remove('form_group-wrong');
                 document.getElementById('group_email').classList.add('form_group-correct'); 
                 document.querySelector('#group_email .form_input-error').classList.remove('form_input-error-active');
+                fields.correo = true;
             }else {
                 document.getElementById('group_email').classList.add('form_group-wrong');
                 document.getElementById('group_email').classList.remove('form_group-correct');
                 document.querySelector('#group_email .form_input-error').classList.add('form_input-error-active');
+                fields.correo = false;
             }
         break;
         case "password":
@@ -57,10 +118,12 @@ var validateform = function validatecampo(e){
                 document.getElementById('group_password').classList.remove('form_group-wrong');
                 document.getElementById('group_password').classList.add('form_group-correct'); 
                 document.querySelector('#group_password .form_input-error').classList.remove('form_input-error-active');
+                fields.password = true;
             }else {
                 document.getElementById('group_password').classList.add('form_group-wrong');
                 document.getElementById('group_password').classList.remove('form_group-correct');
                 document.querySelector('#group_password .form_input-error').classList.add('form_input-error-active');
+                fields.password = false;
             }
         break;
         case "password2":
@@ -74,10 +137,12 @@ var validateform = function validatecampo(e){
                 document.getElementById('group_TelephoneNumber').classList.remove('form_group-wrong');
                 document.getElementById('group_TelephoneNumber').classList.add('form_group-correct'); 
                 document.querySelector('#group_TelephoneNumber .form_input-error').classList.remove('form_input-error-active');
+                fields.phone = true;
             }else {
                 document.getElementById('group_TelephoneNumber').classList.add('form_group-wrong');
                 document.getElementById('group_TelephoneNumber').classList.remove('form_group-correct');
                 document.querySelector('#group_TelephoneNumber .form_input-error').classList.add('form_input-error-active');
+                fields.phone = false;
             }
         break;
         case "Adress":
@@ -85,10 +150,12 @@ var validateform = function validatecampo(e){
                 document.getElementById('group_Adress').classList.remove('form_group-wrong');
                 document.getElementById('group_Adress').classList.add('form_group-correct'); 
                 document.querySelector('#group_Adress .form_input-error').classList.remove('form_input-error-active');
+                fields.nameadress = true;
             }else {
                 document.getElementById('group_Adress').classList.add('form_group-wrong');
                 document.getElementById('group_Adress').classList.remove('form_group-correct');
                 document.querySelector('#group_Adress .form_input-error').classList.add('form_input-error-active');
+                fields.nameadress = false;
             }
         break;
         case "city":
@@ -96,10 +163,12 @@ var validateform = function validatecampo(e){
                 document.getElementById('group_city').classList.remove('form_group-wrong');
                 document.getElementById('group_city').classList.add('form_group-correct'); 
                 document.querySelector('#group_city .form_input-error').classList.remove('form_input-error-active');
+                fields.capitacity = true;
             }else {
                 document.getElementById('group_city').classList.add('form_group-wrong');
                 document.getElementById('group_city').classList.remove('form_group-correct');
                 document.querySelector('#group_city .form_input-error').classList.add('form_input-error-active');
+                fields.capitacity = false;
             }
         break;
         case "postalcode":
@@ -107,10 +176,12 @@ var validateform = function validatecampo(e){
                 document.getElementById('group_postalcode').classList.remove('form_group-wrong');
                 document.getElementById('group_postalcode').classList.add('form_group-correct'); 
                 document.querySelector('#group_postalcode .form_input-error').classList.remove('form_input-error-active');
+                fields.codepostal = true;
             }else {
                 document.getElementById('group_postalcode').classList.add('form_group-wrong');
                 document.getElementById('group_postalcode').classList.remove('form_group-correct');
                 document.querySelector('#group_postalcode .form_input-error').classList.add('form_input-error-active');
+                fields.codepostal = false;
             }
         break;
         case "dni":
@@ -118,10 +189,12 @@ var validateform = function validatecampo(e){
                 document.getElementById('group_dni').classList.remove('form_group-wrong');
                 document.getElementById('group_dni').classList.add('form_group-correct'); 
                 document.querySelector('#group_dni .form_input-error').classList.remove('form_input-error-active');
+                fields.identidy = true;
             }else {
                 document.getElementById('group_dni').classList.add('form_group-wrong');
                 document.getElementById('group_dni').classList.remove('form_group-correct');
                 document.querySelector('#group_dni .form_input-error').classList.add('form_input-error-active');
+                fields.identidy = false;
             }
         break;
     }
@@ -137,10 +210,12 @@ function validatepassword2(){
         document.getElementById('group_password2').classList.remove('form_group-wrong');
         document.getElementById('group_password2').classList.add('form_group-correct');
         document.querySelector('#group_password2 .form_input-error').classList.remove('form_input-error-active');
+        fields.password = true;
     } else {
         document.getElementById('group_password2').classList.add('form_group-wrong');
         document.getElementById('group_password2').classList.remove('form_group-correct');
         document.querySelector('#group_password2 .form_input-error').classList.add('form_input-error-active');
+        fields.password = false;
     }
 }
 
@@ -152,10 +227,12 @@ function validateage(){
         document.getElementById('group_age').classList.remove('form_group-wrong');
         document.getElementById('group_age').classList.add('form_group-correct');
         document.querySelector('#group_age .form_input-error').classList.remove('form_input-error-active');
+        fields.ages = true;
     } else {
         document.getElementById('group_age').classList.add('form_group-wrong');
         document.getElementById('group_age').classList.remove('form_group-correct');
         document.querySelector('#group_age .form_input-error').classList.add('form_input-error-active');
+        fields.ages = false;
     }
 }
 
@@ -166,10 +243,48 @@ inputs.forEach(function validateinputs(input){
     input.addEventListener('focus', validateform2);
 })
 
-
-
-
-
-formulario.addEventListener('submit', function validate(e){
+formulario.addEventListener('submit', function formu(e){
     e.preventDefault();
-});
+    
+    if(fields.completename && fields.correo && fields.password && fields.phone && fields.nameadress && fields.identidy && fields.codepostal && fields.ages && fields.capitacity){
+        
+        
+
+        document.getElementById('form_message-sucess').classList.add('form_message-sucess-active');
+        setTimeout(() => {
+            document.getElementById('form_message-sucess').classList.remove('form_message-sucess-active');
+        }, 5000);
+
+        var fullname = document.getElementById('namecomplete').value;
+        var fullemail = document.getElementById('email').value;
+        var fullpassword = document.getElementById('password').value;
+        var fullage = document.getElementById('age').value;
+        var fullphone = document.getElementById('TelephoneNumber').value;
+        var fulladress = document.getElementById('Adress').value;
+        var fullcity = document.getElementById('city').value;
+        var fullpostalcode = document.getElementById('postalcode').value;
+        var fulldni = document.getElementById('dni').value;
+
+
+        alert('Name: ' + fullname + ';' + ' Email: ' + fullemail + ';' + ' Password: ' + fullpassword + ';' + ' Age: ' + fullage 
+        + ';' + ' Telephone: ' + fullphone + ';' + ' Adress: ' + fulladress + ';' + ' City: ' + fullcity + ';' + ' Postal Code ' + fullpostalcode + ';' + ' DNI: ' + fulldni);
+
+
+        formulario.reset(); 
+
+        document.querySelectorAll('.form_group-correct').forEach((enter) =>{
+            enter.classList.remove('form_group-correct')
+        })
+
+        
+
+    } else {
+        document.getElementById('form_message').classList.add('form_message-active');
+    }
+
+
+
+
+
+
+})
